a826d6a846a7ed3c146a8960da46a5f2ae1cc7b7
Tony Heydorf <ashyiscool2@gmail.com>
2021-05-18 20:37:33 -0400

    changelog update

--- resources/js/pages/Change.vue
+++ resources/js/pages/Change.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="container">
-      <ul style="color:black; font-weight:bolder">
-            <u>May, 17, 2021</u>
-          <li>
-              Version 1.0.0 (Beta) Launches
-          </li>
-          <li>
-              Pushed some updates to mobile layout. It's a work in progress, but mobile should
-              be looking good by the end of the week.
-          </li>
-      </ul>
+    <ul style="color: black; font-weight: bolder">
+      <u>May, 18, 2021</u>
+      <li>
+        implemented notification when a tweet is quoting another and showing
+        context
+      </li>
+      <li>removed redundant media links</li>
+
+      <hr>
+
+      <u>May, 17, 2021</u>
+      <li>Version 1.0.0 (Beta) Launches</li>
+      <li>
+        Pushed some updates to mobile layout. It's a work in progress, but
+        mobile should be looking good by the end of the week.
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
