3ee6dd331dfff0c74e1ee1cfbac5f8a9920a5bed
Tony Heydorf <ashyiscool2@gmail.com>
2021-05-18 21:34:04 -0400

    '

--- database/migrations/2021_05_02_012509_create_tweets_table.php
+++ database/migrations/2021_05_02_012509_create_tweets_table.php
@@ -29,9 +29,9 @@ class CreateTweetsTable extends Migration
             $table->integer('status_retweet_count');
             $table->integer('status_favorite_count');
             $table->string('status_media_url')->nullable();
-            $table->string('status_urls')->nullable();
+            $table->text('status_urls')->nullable();
             $table->string('status_parent')->nullable();
-            $table->text('quoted_url')->nullable();
+            $table->string('quoted_url')->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
